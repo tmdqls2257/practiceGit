5eed6e6145dbf17ae17f4863f68036cecfcfd792
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-02 08:24:22 +0900

    💄  두번째 파일

---
+++ gittest2.ts
@@ -0,0 +1,14 @@
+function test4(){
+    console.log('main branch에서 온 아이입니다.');
+    console.log('두번째 커밋입니다.');
+}
+test1()
+function test5(){
+    console.log('두번째 branch입니다.');
+    console.log('두번째 커밋입니다.');
+}
+test2()
+function test6(){
+    console.log('세번째 branch입니다.');
+}
+test3()
