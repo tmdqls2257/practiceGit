9c26719c67571b25033256dde43195d375120a7d
tmdqls2257 <tmdqls2257@gmail.com>
2022-08-02 08:22:00 +0900

    💄 three branch

--- gittest.ts
+++ gittest.ts
@@ -2,7 +2,13 @@ function test1(){
     console.log('main branch에서 온 아이입니다.');
     console.log('두번째 커밋입니다.');
 }
+test1()
 function test2(){
     console.log('두번째 branch입니다.');
     console.log('두번째 커밋입니다.');
 }
+test2()
+function test3(){
+    console.log('세번째 branch입니다.');
+}
+test3()
